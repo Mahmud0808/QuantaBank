5d4d13b11d49cd15e00ce6b491969592f17e8ace
DrDisagree <crazymahmud08@gmail.com>
2024-07-10 15:30:49 +0600

    Implement layout structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,32 @@
+import MobileNav from "@/components/layout/MobileNav";
+import SideBar from "@/components/layout/SideBar";
+import Image from "next/image";
+
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const loggedIn = { firstName: "Mahmud", lastName: "Hasan" };
+
     return (
-        <main>
-            {children}
+        <main className="flex h-screen w-full font-inter">
+            <SideBar user={loggedIn} />
+            <div className="flex flex-col size-full">
+                <div className="root-layout">
+                    <Image
+                        src="/icons/logo.svg"
+                        width={30}
+                        height={30}
+                        alt="logo"
+                        className="max-xl:size-14"
+                    />
+                    <div>
+                        <MobileNav user={loggedIn} />
+                    </div>
+                </div>
+                {children}
+            </div>
         </main>
     );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import HeaderBox from "@/components/layout/HeaderBox";
+import RightSideBar from "@/components/layout/RightSideBar";
+import TotalBalanceBox from "@/components/layout/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+    const loggedIn = {
+        firstName: "Mahmud",
+        lastName: "Hasan",
+        email: "contact@example.com",
+    };
+
+    return (
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || "Guest"}
+                        subtext="Access and manage your account and transactions efficiently."
+                    />
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.35}
+                    />
+                </header>
+
+                RECENT TRANSACTIONS
+            </div>
+            <RightSideBar
+                user={loggedIn}
+                transactions={[]}
+                banks={[{ currentBalance: 123.5 }, { currentBalance: 500.75 }]}
+            />
+        </section>
+    );
+};
 
 export default Home;
